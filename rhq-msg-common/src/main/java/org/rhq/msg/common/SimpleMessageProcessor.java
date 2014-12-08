04672ace0179cb5b970fa97390262c479695b2d6
John Mazzitelli <mazz@redhat.com>
2014-12-08 16:25:25 -0500

    reformat

--- rhq-msg-common/src/main/java/org/rhq/msg/common/SimpleMessageProcessor.java
+++ rhq-msg-common/src/main/java/org/rhq/msg/common/SimpleMessageProcessor.java
@@ -12,21 +12,21 @@ import org.rhq.msg.common.consumer.RPCConnectionContext;
 import org.rhq.msg.common.producer.ProducerConnectionContext;
 
 /**
- * A version of the MessageProcessor that keeps he passed interfaces internally
+ * A version of the MessageProcessor that keeps the passed interfaces internally.
+ * 
  * @author Heiko W. Rupp
  */
 public class SimpleMessageProcessor extends MessageProcessor {
 
-    private ConsumerConnectionContext consumerCtx;
-    private ProducerConnectionContext producerCtx;
+    private final ConsumerConnectionContext consumerCtx;
+    private final ProducerConnectionContext producerCtx;
 
     public SimpleMessageProcessor(ConsumerConnectionContext consumerCtx, ProducerConnectionContext producerCtx) {
         this.consumerCtx = consumerCtx;
         this.producerCtx = producerCtx;
     }
 
-    public <T extends BasicMessage> void listen(
-                                                AbstractBasicMessageListener<T> listener) throws JMSException {
+    public <T extends BasicMessage> void listen(AbstractBasicMessageListener<T> listener) throws JMSException {
         super.listen(consumerCtx, listener);
     }
 
@@ -34,16 +34,12 @@ public class SimpleMessageProcessor extends MessageProcessor {
         return super.send(producerCtx, basicMessage);
     }
 
-    public <T extends BasicMessage> RPCConnectionContext sendAndListen(
-                                                                       BasicMessage basicMessage,
-                                                                       BasicMessageListener<T> responseListener) throws JMSException {
+    public <T extends BasicMessage> RPCConnectionContext sendAndListen(BasicMessage basicMessage, BasicMessageListener<T> responseListener) throws JMSException {
         return super.sendAndListen(producerCtx, basicMessage, responseListener);
     }
 
-    public <R extends BasicMessage> Future<R> sendRPC(BasicMessage basicMessage,
-                                                      Class<R> expectedResponseMessageClass) throws JMSException {
-        return super.sendRPC(producerCtx, basicMessage,
-            expectedResponseMessageClass);
+    public <R extends BasicMessage> Future<R> sendRPC(BasicMessage basicMessage, Class<R> expectedResponseMessageClass) throws JMSException {
+        return super.sendRPC(producerCtx, basicMessage, expectedResponseMessageClass);
     }
 
     protected Message createMessage(ConnectionContext context, BasicMessage basicMessage) throws JMSException {
